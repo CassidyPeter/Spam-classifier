740cd847eb223b4d7922d231192f94ecea276a47
Peter Cassidy <peter_c123@live.com>
2020-07-06 20:52:48 +0100

    Added top predictors

--- Main.m
+++ Main.m
@@ -11,7 +11,18 @@ sample1 = readFile('spamSample1.txt');
 sample2 = readFile('spamSample2.txt');
 sample3 = readFile('spamSample3.txt');
 sample4 = readFile('nonSpamEmail.txt');
-sample1pred = predict(model, emailFeatures(processEmail(sample1))')
-sample2pred = predict(model, emailFeatures(processEmail(sample2))')
-sample3pred = predict(model, emailFeatures(processEmail(sample3))')
-sample4pred = predict(model, emailFeatures(processEmail(sample4))')
+sample1pred = predict(model, emailFeatures(processEmail(sample1))') %spam
+sample2pred = predict(model, emailFeatures(processEmail(sample2))') %spam
+sample3pred = predict(model, emailFeatures(processEmail(sample3))') %spam
+sample4pred = predict(model, emailFeatures(processEmail(sample4))') %not-spam (ham)
+
+%Top predictors of spam
+[weight, idx] = sort(model.Beta, 'descend');
+vocabList = getVocabList();
+for i=1:20
+  if i==1
+    fprintf('Top predictors of spam: \n');
+  end
+  fprintf('%-15s (%f) \n', vocabList{idx(i)}, weight(i));
+end
+
